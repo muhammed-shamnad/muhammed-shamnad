fbd8440aaeae3f0afae74a56631fc7643223e0ea
Muhammed Shamnad <muhammed.shamnad@factweavers.com>
2022-10-21 15:55:20 +0530

    develop cleanup

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-  const express = require('express');
-  const router = express.Router();
-
-  const s3HandlingRoutes = require('./s3-Handling-Routes')
-
-  router.use('/s3', s3HandlingRoutes);
-  
-  module.exports = router;
